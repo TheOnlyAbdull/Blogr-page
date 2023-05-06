dca8d8c2ec1293ea1666e3972e36823a2b945509
Abdullahi Salaudeen <abdullahisalaudeen65@gmail.com>
2023-05-06 13:03:37 +0100

    navigation bar functionality

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 //selecting element from the DOM
 const dropDowns = document.querySelectorAll('.drop-down');
+const navBtn = document.querySelector('.menu-bar');
+const nav = document.querySelector('.navigate');
+const navLink = document.querySelector('.nav-ul');
+const logLink = document.querySelector('.log-ul');
+const imgSrc = document.getElementById('img-src');
 
-//For each dropdown
+
+
+//For each nav dropdown
 dropDowns.forEach(dropDown => {
   const dropContent = dropDown.querySelector('.drop-down-content');
 
-// Add click event listener to the dropdown menu
+// Add click event listener to the dropdown to each nav
   dropDown.addEventListener('click', () => {
       dropContent.classList.toggle('show-dropdown');
   });
@@ -17,3 +24,26 @@ dropDowns.forEach(dropDown => {
       }
   });
 });
+
+//Add click event to the navigation bar
+navBtn.addEventListener('click', () => {
+
+  // toggle full navigation
+  nav.classList.toggle('show-nav');
+  navLink.classList.toggle('display-nav');
+  logLink.classList.toggle('display-nav');
+/*
+//getting current scr
+const currentSrc = imgSrc.getAttribute("src");
+
+//check for current src and change to the other
+if (currentSrc === "images\icon-hamburger.svg") {
+  imgSrc.setAttribute("src","images\icon-close.svg");
+}else{
+  imgSrc.setAttribute("src", "images\icon-hamburger.svg");
+}
+*/
+});
+
+
+
